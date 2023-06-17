9c3423ae880b49de25dc349f50c0d1193a9b8ba6
Gabriel Henrique Alves <100585280+gb-alves03@users.noreply.github.com>
2023-06-17 10:07:08 -0300

    Update Fahrenheit.java

--- src/tempConverter/Fahrenheit.java
+++ src/tempConverter/Fahrenheit.java
@@ -1,18 +1,16 @@
 package tempConverter;
+
 import java.util.Scanner;
 
-public class Fahrenheit {
-	private double temperature;
+public class Fahrenheit extends Temperature{
 	
 	Scanner input = new Scanner(System.in);
 	
-	public void setTemperature(double temperature) {
-		this.temperature = temperature;
-	}
-	public double getTemperature() {
-		return temperature;
+	public Fahrenheit(double temperature) {
+		super(temperature);
 	}
 	
+	@Override
 	public void insertTemperature() {
 		System.out.println("Insert the temperature in Fahrenheit to convert: ");
 		this.temperature = input.nextDouble();
