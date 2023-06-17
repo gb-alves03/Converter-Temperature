a043eba28fbc768c0d9a5e7c799bc6feaf66a459
Gabriel Henrique Alves <100585280+gb-alves03@users.noreply.github.com>
2023-06-17 10:04:48 -0300

    Update Main.java

--- src/tempConverter/Main.java
+++ src/tempConverter/Main.java
@@ -3,26 +3,36 @@ package tempConverter;
 
 public class Main {
 	public static void main(String[] args) {
+		
 		Celsius celsius = new Celsius();
 		celsius.insertTemperature();
 		
 		Fahrenheit fahrenheit = new Fahrenheit();
 		fahrenheit.insertTemperature();
 		
+		Rankine rankine = new Rankine();
+		rankine.insertTemperature();
+		
 		Kelvin kelvin = new Kelvin();
 		kelvin.insertTemperature();
 		
-		//System.out.println("A temperatura é: " + celsius.getTemperature());
 		Converter converter = new Converter();
 		converter.setCelsius(celsius);
 		converter.setFahrenheit(fahrenheit);
 		converter.setKelvin(kelvin);
+		converter.setRankine(rankine);
 		
 		converter.celsiusToFahrenheit();
 		converter.celsiusToKelvin();
+		converter.celsiusToRankine();
 		converter.fahrenheitToCelsius();
 		converter.fahrenheitToKelvin();
+		converter.fahrenheitToRankine();
 		converter.kelvinToCelsius();
 		converter.kelvinToFahrenheit();
+		converter.kelvinToRankine();
+		converter.rankineToCelsius();
+		converter.rankineToFahrenheit();
+		converter.rankineToKelvin();
 	}
 }
