a499d75b0e31ed5cc9c18dfb697e7e420a8ca4b7
Gabriel Henrique Alves <100585280+gb-alves03@users.noreply.github.com>
2023-06-17 10:06:47 -0300

    Update Celsius.java

--- src/tempConverter/Celsius.java
+++ src/tempConverter/Celsius.java
@@ -1,18 +1,16 @@
 package tempConverter;
+
 import java.util.Scanner;
 
-public class Celsius {
-	private double temperature;
+public class Celsius extends Temperature{
 	
 	Scanner input = new Scanner(System.in);
 	
-	public void setTemperature(double temperature) {
-		this.temperature = temperature;
-	}
-	public double getTemperature() {
-		return temperature;
+	public Celsius(double temperature) {
+		super(temperature);
 	}
 	
+	@Override
 	public void insertTemperature() {
 		System.out.println("Insert the temperature in Celsius to convert: ");
 		this.temperature = input.nextDouble();
